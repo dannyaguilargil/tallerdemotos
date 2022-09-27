bd954a7a7b160b39926ed224da045ad7a06c9ff7
dannyaguilargilGH <52986710+dannyaguilargil@users.noreply.github.com>
2022-09-26 19:51:03 -0500

    Creaccion de los archivos
    Creacccion del repositorio

---
+++ index.php
@@ -0,0 +1,32 @@
+<!--CONVERTIDOR -> https://khaledkzy.github.io/pixel-vh-vw-converter/ -->
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    
+    <link rel="icon" href="imgs/motocross.ico">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Home</title>
+</head>
+<body>
+   
+   
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
